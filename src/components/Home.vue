2b60de2f3c2f06e821864efaac0c75ccd7e15cb9
RichardRiss <Niwatori93>
2022-02-15 17:19:27 +0100

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+// Home.vue
+
+<template>
+<div>
+    <p>Home Page</p>
+    <p>Random Number from Backend: {{ randomNumber }}</p>
+    <button @click="getRandom">Random Number Generator</button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            randomNumber: 0
+        }
+    },
+
+    methods: {
+        getRandom() {
+            this.randomNumber = this.GetFromBackend()
+        },
+        GetFromBackend() {
+            const Arr_basepath = window.location.href.split(':')
+            const basepath = Arr_basepath[0] + ":" + Arr_basepath[1]
+            const path = basepath + `:5000/api/random`
+            console.log(path)
+            const axios = require('axios')
+            axios.get(path)
+                .then((response) => {
+                    this.randomNumber = response.data.randomNumber
+                    console.log(response.data.randomNumber)
+                })
+                .catch((error) => {
+                    console.log(error)
+                    this.randomNumber = error
+                })
+        }
+    }
+}
+</script>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,6 @@
+//NotFound.vue
+<template>
+  <div>
+      <p> 404 Error Not Found </p>
+  </div>
+</template>
