dff28059b418f461e62d444184ff3d6a350056e8
RichardRiss <Niwatori93>
2022-02-27 22:57:13 +0100

    added vue-tailwind

---
+++ src/components/SingleMotor.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h3> Landing Page of Device {{DeviceName}} </h3>
+    <desy-button>Desy colored button</desy-button>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      DeviceName: 'Testmotor'
+    }
+  }
+}
+</script>
